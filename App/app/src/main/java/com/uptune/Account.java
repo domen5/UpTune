072ff209d4726c974ab6a7ac1e87668e52f61db6
LeleDallas <71103219+LeleDallas@users.noreply.github.com>
2021-04-27 02:19:05 +0200

    Added loading window, login singUp forms, navbar

---
+++ App/app/src/main/java/com/uptune/Account.java
@@ -0,0 +1,48 @@
+package com.uptune;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        //region Nav Bottom
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav);
+        bottomNavView.setSelectedItemId(R.id.user);
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class)); //start
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.charts:
+                        startActivity(new Intent(getApplicationContext(), Chart.class)); //start
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.search:
+                        startActivity(new Intent(getApplicationContext(), Catalog.class)); //start
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.user:
+                        startActivity(new Intent(getApplicationContext(), Account.class)); //start
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+        //endregion
+    }
+}
---
+++ App/app/src/main/java/com/uptune/Login.java
@@ -0,0 +1,55 @@
+package com.uptune;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        Button callSingUp = findViewById(R.id.sing_up);
+        Button callLogIn = findViewById(R.id.logIn);
+
+        ImageView img= findViewById(R.id.logo);
+        TextView loginTitle= findViewById(R.id.LoginTitle);
+        TextView loginText= findViewById(R.id.LoginText);
+        TextInputLayout username= findViewById(R.id.username);
+        TextInputLayout password= findViewById(R.id.password);
+
+
+
+        callSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SingUp.class);
+                Pair[ ] pairs = new Pair[7];
+
+                pairs[0]=new Pair<View,String> (img, "logo_img");
+                pairs[1]=new Pair<View,String> (loginTitle, "loadTitle");
+                pairs[2]=new Pair<View,String> (loginText, "loadText");
+                pairs[3]=new Pair<View,String> (username, "loadUsername");
+                pairs[4]=new Pair<View,String> (password, "loadPw");
+                pairs[5]=new Pair<View,String> (callSingUp, "loadLogin");
+                pairs[6]=new Pair<View,String> (callLogIn, "loadSing");
+                ActivityOptions opt = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+                startActivity(intent, opt.toBundle());
+            }
+        });
+    }
+}
