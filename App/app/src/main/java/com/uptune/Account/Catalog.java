e51b874f131087946f7bfb1018c1bdc45d35a610
LeleDallas <71103219+LeleDallas@users.noreply.github.com>
2021-04-29 12:42:05 +0200

    Added first spotify call

--- App/app/src/main/java/com/uptune/Account/Catalog.java
+++ App/app/src/main/java/com/uptune/Account/Catalog.java
@@ -8,55 +8,88 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.common.api.Response;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.uptune.Helper.CardAdapter;
 import com.uptune.Helper.CardContainer;
 import com.uptune.MainActivity;
 import com.uptune.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.AlbumSimple;
+import kaaes.spotify.webapi.android.models.AlbumsPager;
 import retrofit.Callback;
 import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class Catalog extends AppCompatActivity {
 
     RecyclerView bestCateg, mostList, mostList2;
     RecyclerView.Adapter adapter, adapter2;
 
+    private static final String CLIENT_ID = "38b6f15617554f428ca3d3c5358deab4";
+    private static final String REDIRECT_URI = "https://api-university.com/";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
         setContentView(R.layout.activity_catalog);
         bestCateg = findViewById(R.id.bestCateg);
         mostList = findViewById(R.id.mostList);
         mostList2 = findViewById(R.id.mostList2);
 
+
         SpotifyApi api = new SpotifyApi();
-        api.setAccessToken("894fc017619f4abaab7aa92fffe65ed8");
+
+        api.setAccessToken("BQAWkQzS8rO6Ec2ImQ9Epcd0M4lQeSxyj8X7EpFTvEGtAAOblc6SANGD4wFxI1bdJDs_BlpWfc3khXbPdqNc0HKM2N_BpvmfzBV1GuBvBjr68Luauz4dnHhAyTF98lGA4GWCyxoEQRAvCEIlWonNMEYmUQCJVGs96yXVKA");
         SpotifyService fetchData = api.getService();
 
-        fetchData.getAlbum("Eminem", new Callback<Album>() {
-            public void success(Album album, Response response) {
-                Log.d("Album success", album.name);
+        /*fetchData.getAlbum("4aawyAB9vmqN3uQ7FjRGTy", new Callback<Album>() {
+            @Override
+            public void success(Album album, retrofit.client.Response response) {
+                Log.i("Success", album.name );
             }
 
             @Override
-            public void success(Album album, retrofit.client.Response response) {
+            public void failure(RetrofitError error) {
+                Log.i("Failure", error.toString());
+            }
+        });
+*/
+        fetchData.searchAlbums("Music to be murder", new Callback<AlbumsPager>() {
+
+            @Override
+            public void success(AlbumsPager albumsPager, Response response) {
+                List<AlbumSimple> list = albumsPager.albums.items;
+                for (AlbumSimple i : list) {
+                    Log.i("Success",i.images.toString() );
+
+                }
+
 
             }
 
             @Override
             public void failure(RetrofitError error) {
-                Log.d("Album failure", error.toString());
+
             }
         });
-
-
 
         renderBestCateg();
         //region Nav Bottom
