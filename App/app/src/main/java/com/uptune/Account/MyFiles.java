0decca32e92063ee7bb5638d8d2746daabc0c647
LeleDallas <71103219+LeleDallas@users.noreply.github.com>
2021-05-19 15:22:56 +0200

    Recycle with our songs

--- App/app/src/main/java/com/uptune/Account/MyFiles.java
+++ App/app/src/main/java/com/uptune/Account/MyFiles.java
@@ -16,109 +16,65 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.uptune.Adapter.CardAdapter;
+import com.uptune.Adapter.UserSongAdapter;
+import com.uptune.Helper.CardContainer;
 import com.uptune.R;
+import com.uptune.Song.SongList;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class MyFiles extends AppCompatActivity {
-    TextView currentTime, totalTime;
-    SeekBar seekBar;
     FirebaseStorage firebaseStorage;
     StorageReference storageReference;
     StorageReference ref;
-    private Handler handler = new Handler();
-    MediaPlayer mediaPlayer;
+    RecyclerView.Adapter adapter;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    FirebaseAuth auth;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_files);
-        Button btn = findViewById(R.id.btn_download);
-        currentTime = findViewById(R.id.current_time);
-        totalTime = findViewById(R.id.total_time);
-        seekBar = findViewById(R.id.player_seek);
-        Button btnPlay = findViewById(R.id.btn_play);
-        Button btnStop = findViewById(R.id.btn_stop);
-        mediaPlayer = new MediaPlayer();
-        btn.setOnClickListener(v -> download());
-        seekBar.setMax(100);
-        btnPlay.setOnClickListener(v -> {
-            if (mediaPlayer.isPlaying()) {
-                handler.removeCallbacks(updater);
-                mediaPlayer.pause();
-            } else {
-                mediaPlayer.start();
-                updateSeek();
-            }
-        });
+        //Get from db
+        auth = FirebaseAuth.getInstance();
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("owned").child("leleshady");
+     //String a= reference.child();
 
-        try {
-            prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        seekBar.setOnTouchListener((v, event) -> {
-            SeekBar seekBar = (SeekBar) v;
-            int pos = mediaPlayer.getDuration() / 100 * seekBar.getProgress();
-            mediaPlayer.seekTo(pos);
-            currentTime.setText(millisToTimer(mediaPlayer.getCurrentPosition()));
-            return false;
-        });
+        ArrayList<SongList> setSongOwned = new ArrayList<>();
+        setSongOwned.add(new SongList("Intro", "1"));
+        setSongOwned.add(new SongList("Shoot", "2"));
+
+        androidx.recyclerview.widget.RecyclerView rv = findViewById(R.id.user_songs_recycler);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        adapter = new UserSongAdapter(setSongOwned, this);
+        rv.setAdapter(adapter);
     }
 
-    private Runnable updater = new Runnable() {
-        @Override
-        public void run() {
-            updateSeek();
-            long currentDuration = mediaPlayer.getCurrentPosition();
-            currentTime.setText(millisToTimer(currentDuration));
-        }
-    };
 
-    private void updateSeek() {
-        if (mediaPlayer.isPlaying()) {
-            seekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
-            handler.postDelayed(updater, 1000);
-        }
-    }
-
-    private String millisToTimer(long millis) {
-        String time = "";
-        String secStr;
-        int hours = (int) (millis / (1000 * 3600));
-        int min = (int) (millis % (1000 * 3600)) / (1000 * 60);
-        int sec = (int) ((millis % (1000 * 3600)) % (1000 * 60) / 1000);
-        if (hours > 0)
-            time = hours + ":";
-        if (sec < 10)
-            secStr = "0" + sec;
-        else
-            secStr = "" + sec;
-        time = time + min + ":" + secStr;
-        Log.i("TIME", time);
-        return time;
-    }
-
-    private void prepare() throws IOException {
-        mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/uptune-2d37c.appspot.com/o/Shoot%20the%20stars%20aim%20for%20the%20moon%2F01%20Bad%20Bitch%20from%20Tokyo%20(Intro).m4a?alt=media&token=5417b3b9-be65-4776-896d-09d4f9790f8d");
-        mediaPlayer.prepare();
-        totalTime.setText(millisToTimer(mediaPlayer.getDuration()));
-        updateSeek();
-    }
 
     private void download() {
-        storageReference = firebaseStorage.getInstance().getReference();
-        ref = storageReference.child("a.pdf");
+        storageReference = firebaseStorage.getInstance().getReference("Shoot the stars aim for the moon/");
+        ref = storageReference.child("01 Bad Bitch from Tokyo (Intro).m4a");
         ref.getDownloadUrl().addOnSuccessListener(uri -> {
             String url = uri.toString();
-            downloadFiles(MyFiles.this, "a", ".pdf", DIRECTORY_DOWNLOADS, url);
+            downloadFiles(MyFiles.this, "01 Bad Bitch from Tokyo (Intro)", ".m4a", DIRECTORY_DOWNLOADS, url);
         }).addOnFailureListener(e -> {
             Toast.makeText(getApplicationContext(), "NO", Toast.LENGTH_LONG).show();
         });
---
+++ App/app/src/main/java/com/uptune/Adapter/UserSongAdapter.java
@@ -0,0 +1,172 @@
+package com.uptune.Adapter;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.uptune.Account.MyFiles;
+import com.uptune.R;
+import com.uptune.Song.SongList;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+public class UserSongAdapter extends RecyclerView.Adapter<UserSongAdapter.FeatureViewHolder> {
+
+    Context context;
+    ArrayList<SongList> location;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    StorageReference ref;
+    MediaPlayer mediaPlayer;
+    int state = -1;
+    private Handler handler = new Handler();
+
+    public UserSongAdapter(ArrayList<SongList> location, Context context) {
+        this.location = location;
+        this.context = context;
+        mediaPlayer = new MediaPlayer();
+    }
+
+    @NonNull
+    @Override
+    public FeatureViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_myfiles, parent, false);
+        FeatureViewHolder fvh = new FeatureViewHolder(v);
+        return fvh;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onBindViewHolder(@NonNull FeatureViewHolder holder, int position) {
+        SongList songList = location.get(position);
+        holder.title.setText(songList.getTitle());
+        holder.btnPlay.setOnClickListener(v -> {
+            if (mediaPlayer.isPlaying()) {
+                handler.removeCallbacksAndMessages(null);
+                mediaPlayer.pause();
+                holder.btnPlay.setImageResource(R.drawable.ic_music_play);
+            } else {
+                if (state != position) {
+                    try {
+                        prepare(holder);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                state = position;
+                mediaPlayer.start();
+                updateSeek(holder);
+                holder.btnPlay.setImageResource(R.drawable.ic_music_pause);
+            }
+        });
+        holder.seekBar.setOnTouchListener((v, event) -> {
+            SeekBar seekBar = (SeekBar) v;
+            int pos = mediaPlayer.getDuration() / 100 * seekBar.getProgress();
+            mediaPlayer.seekTo(pos);
+            holder.currentTime.setText(millisToTimer(mediaPlayer.getCurrentPosition()));
+            return false;
+        });
+        holder.btnDownload.setOnClickListener(v -> download());
+    }
+
+    private void updateSeek(FeatureViewHolder holder) {
+        if (mediaPlayer.isPlaying()) {
+            holder.seekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
+            handler.postDelayed(() -> {
+                long currentDuration = mediaPlayer.getCurrentPosition();
+                holder.currentTime.setText(millisToTimer(currentDuration));
+            }, 1000);
+        }
+    }
+
+    private String millisToTimer(long millis) {
+        String time = "";
+        String secStr;
+        int hours = (int) (millis / (1000 * 3600));
+        int min = (int) (millis % (1000 * 3600)) / (1000 * 60);
+        int sec = (int) ((millis % (1000 * 3600)) % (1000 * 60) / 1000);
+        if (hours > 0)
+            time = hours + ":";
+        if (sec < 10)
+            secStr = "0" + sec;
+        else
+            secStr = "" + sec;
+        time = time + min + ":" + secStr;
+        Log.i("TIME", time);
+        return time;
+    }
+
+    private void prepare(FeatureViewHolder holder) throws IOException {
+        mediaPlayer.reset();
+        mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/uptune-2d37c.appspot.com/o/Shoot%20the%20stars%20aim%20for%20the%20moon%2F01%20Bad%20Bitch%20from%20Tokyo%20(Intro).m4a?alt=media&token=5417b3b9-be65-4776-896d-09d4f9790f8d");
+        mediaPlayer.prepare();
+        holder.totalTime.setText(millisToTimer(mediaPlayer.getDuration()));
+        updateSeek(holder);
+    }
+
+    private void download() {
+        storageReference = firebaseStorage.getInstance().getReference("Shoot the stars aim for the moon/");
+        ref = storageReference.child("01 Bad Bitch from Tokyo (Intro).m4a");
+        ref.getDownloadUrl().addOnSuccessListener(uri -> {
+            String url = uri.toString();
+            downloadFiles(this.context, "01 Bad Bitch from Tokyo (Intro)", ".m4a", DIRECTORY_DOWNLOADS, url);
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this.context, "NO", Toast.LENGTH_LONG).show();
+        });
+    }
+
+    private void downloadFiles(Context context, String fileName, String ext, String dest, String url) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request req = new DownloadManager.Request(uri);
+        req.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        req.setDestinationInExternalFilesDir(context, dest, fileName + ext);
+        downloadManager.enqueue(req);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return location.size();
+    }
+
+
+    public static class FeatureViewHolder extends RecyclerView.ViewHolder {
+        TextView title, currentTime, totalTime;
+        ImageButton btnPlay, btnDownload;
+        SeekBar seekBar;
+
+        public FeatureViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.trackNameSong);
+            currentTime = itemView.findViewById(R.id.current_time);
+            totalTime = itemView.findViewById(R.id.total_time);
+            seekBar = itemView.findViewById(R.id.player_seek);
+            btnDownload = itemView.findViewById(R.id.btn_download);
+            btnPlay = itemView.findViewById(R.id.btn_play);
+            seekBar.setMax(100);
+        }
+    }
+}
